dcde6cb301e4a3b2e4da1abf1cb84a310587c496
Bayzman <bassmaninno102@gmail.com>
2024-10-30 06:36:36 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+""" Basic caching system """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic caching system """
+    def __init__(self):
+        """ Constructor of the class """
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """ adds items to the cache """
+        if (key or item) is None:
+            pass
+
+        self.cache_data.update({key: item})
+
+    def get(self, key):
+        """ gets the value at the specified key """
+        if (key is None) or (not key):
+            return None
+
+        return self.cache_data.get(key)
