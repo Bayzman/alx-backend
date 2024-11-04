bf646ae8bc8b1bd07b0eb3d960e0d526909c6faa
Bayzman <bassmaninno102@gmail.com>
2024-11-04 14:47:42 +0000

    Simple heleper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,8 +5,8 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
-    """ Returns a turn containing a start index and 
-        an end index corresponding 
+    """ Returns a turn containing a start index and
+        an end index corresponding
     """
     if (page == 1):
         start_idx, end_idx = (page - 1, page_size)
